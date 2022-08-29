506bbe50011473ef3ba96340c1e64c8ae2a87758
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2022-08-29 19:54:46 +0100

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import { logo } from "../utils/constants";
 
-export default Navbar
+const Navbar = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}>
+    <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+  </Stack>
+)
+
+
+export default Navbar;
